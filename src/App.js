69770edbd6da0b4747e3c8a90adee173d5041829
phoenixzero4 <phoenixzero4@hotmail.com>
2021-05-18 21:09:02 -0700

    added cancel buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-
+import {useState} from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "So far react is very confusing",
+      day: "May 20",
+      reminder: true,
+    },
+    { 
+      id: 2,
+      text: "Meeting with Dr. Borum",
+      day: "May 19",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Headshots at Okanagan College",
+      day: "June 3",
+      reminder: false,
+    }
+  ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id != id));
+  }
 
   return (
   <div className="container">
    <Header />
-   
-   </div>
+   {tasks.length > 0 ? (
+   <Tasks tasks={tasks} onDelete={deleteTask} />
+   ): ( 'No tasks to Show')}  </div>
   )
 }
 // class App extends React.Component{
